c8e2e7ea408aca8f9c9cf252a1ce7cc67cfa59c6
Eyal <87993877+eyal78@users.noreply.github.com>
2022-07-10 14:40:55 +0300

    Create Variables.cpp

---
+++ Variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello Eyal!\n";
+    int x=10;
+    double y=5.5;
+    char a='t';
+    string name="Eyal";
+    bool f=false;
+    cout<< "x="<<x <<" y="<<y<<"\n";
+    cout<< "a="<<a <<" name="<<name<<"\n";
+    cout<< "f="<<f;
+}
