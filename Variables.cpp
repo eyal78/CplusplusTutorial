113c28e9cb1c596cc83ebd7c6b967da71d578e6c
Eyal <87993877+eyal78@users.noreply.github.com>
2022-07-10 22:17:18 +0300

    Update Variables.cpp

--- Variables.cpp
+++ Variables.cpp
@@ -5,12 +5,14 @@ using namespace std;
 int main()
 {
     cout<<"Hello Eyal!\n";
-    int x=10;
+    int x, x1=10, x2=20;
     double y=5.5;
+    const float PI = 3.14;
     char a='t';
     string name="Eyal";
     bool f=false;
-    cout<< "x="<<x <<" y="<<y<<"\n";
+    x=x1+x2;
+    cout<< "x="<<x <<" y="<<y*PI<<"\n";
     cout<< "a="<<a <<" name="<<name<<"\n";
     cout<< "f="<<f;
 }
