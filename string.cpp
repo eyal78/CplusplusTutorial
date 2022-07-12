9688078bec4e288cae276ba7030fe8811a1be4e7
Eyal <87993877+eyal78@users.noreply.github.com>
2022-07-12 22:39:57 +0300

    Update string.cpp

--- string.cpp
+++ string.cpp
@@ -5,6 +5,10 @@ int main ()
 {
   string a = "My name is Eyal, ";
   string b = "I live in beer sheva.";
+  string txt ="My name is Eyal and I live in beer sheva.";
   string c = a + b;
+  cout << txt.length();                    //.length() == .size()
+  cout<<"\n";
+  cout << txt.size();
   cout << c;
 }
