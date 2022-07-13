3f35192c5c94ca37752a30ab12d08530d44f7fcf
Eyal <87993877+eyal78@users.noreply.github.com>
2022-07-13 22:45:27 +0300

    Update string.cpp

--- string.cpp
+++ string.cpp
@@ -6,6 +6,7 @@ int main()
   string a = "My name is Eyal, ";
   string b = "I live in beer sheva.";
   string txt ="My name is Eyal and I live in beer sheva.";
+  string fullName;
   string c = a + b;
   cout << txt.length();                    //.length() == .size()
   cout<<"\n";
@@ -17,4 +18,9 @@ int main()
   cout << "\n";
   c[0]='H';
   cout << c;
+  cout << "\n";
+  cout << "Type your full name: ";
+  getline (cin, fullName);                // function getline - Allows absorption of an entire line and not just one word.
+  cout << "Your name is: " << fullName;
 }
+
