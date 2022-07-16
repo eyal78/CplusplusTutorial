ebb3a696adf561f93e455d541870042ef06c12d4
Eyal <87993877+eyal78@users.noreply.github.com>
2022-07-16 22:36:14 +0300

    Update Math.cpp

--- Math.cpp
+++ Math.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+  cout << "Hello Eyal!\n";
+  cout << sqrt(25);         //print 5
+  cout << "\n";
+  cout << round(2.6);       //print 3
+  cout << "\n";
+  cout << log(2);           //print 0.693147
+  cout << "\n";
+  cout << cos(37);
+  cout << "\n";
+  cout << sin(30);
+  cout << "\n";
+  cout << tan(60);
+}
 
