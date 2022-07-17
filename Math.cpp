ff50eb2994d7546bd31dd8db3c4830a482775aaa
Eyal <87993877+eyal78@users.noreply.github.com>
2022-07-17 22:04:06 +0300

    Update Math.cpp

--- Math.cpp
+++ Math.cpp
@@ -5,21 +5,20 @@ using namespace std;
 
 int main()
 {
-  cout << "Hello Eyal!\n";
+  cout << pow(2, 3);       // print 8(=2^3)
+  cout << "\n";
   cout << sqrt(25);         //print 5
-  cout << "\n";
-  cout << round(2.6);       //print 3
-  cout << "\n";
-  cout << log(2);           //print 0.693147
   cout << "\n";
   cout << cos(37);
   cout << "\n";
   cout << sin(30);
   cout << "\n";
   cout << tan(60);
-  cout << "\n";   
+  cout << "\n"; 
+  cout << round(2.6);       //print 3
+  cout << "\n";
   cout << floor(20.23);    //print 20
   cout << "\n";
-  cout << pow(2, 3);       // print 8(=2^3)
-}
+  cout << log(2);           //print 0.693147
+ }  
 
