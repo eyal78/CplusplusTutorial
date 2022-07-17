b7762fe6a16cbb4bcb6032ac3944bcbd2bdbc6ed
Eyal <87993877+eyal78@users.noreply.github.com>
2022-07-17 22:30:22 +0300

    Update if-else.cpp

--- if-else.cpp
+++ if-else.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x,y,max;
+	cout << "Type a number 1:";
+	cin >> x;
+	cout << "Type a number 2:";
+	cin >> y;
+	if(x>y)
+	{
+		max=x;
+		cout << "The x is bigger.";
+	}
+	else if(x!=y)
+	{
+		max=y;
+		cout << "The y is bigger.";
+	}
+	else
+  {
+		cout << max <<"\n";
+	  cout << "x==y";
+  }
+}
 
