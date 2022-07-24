62d3c5e6654e1814f46f8b8e04016bcbdc2e51aa
Eyal <87993877+eyal78@users.noreply.github.com>
2022-07-24 09:01:02 +0300

    Update Functions.cpp

--- Functions.cpp
+++ Functions.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+void myFunction()
+{
+  cout << "my name is Eyal!\n";
+}
+
+void myFunction(string name)
+{
+	cout << "my name is "<<name<<"\n";
+}
+int main()
+{
+	myFunction();
+	myFunction("Eyal");
+
+}
