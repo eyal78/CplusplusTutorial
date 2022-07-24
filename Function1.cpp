eb5a9f1fff32e5f7e1b07161b5a4a6fb67f4b7ca
Eyal <87993877+eyal78@users.noreply.github.com>
2022-07-24 09:16:36 +0300

    Update and rename Functions.cpp to Function1.cpp

--- Function1.cpp
+++ Function1.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
 using namespace std;
-
+//
 void myFunction()
 {
   cout << "my name is Eyal!\n";
 }
-
+//
 void myFunction(string name)
 {
 	cout << "my name is "<<name<<"\n";
 }
+//
+int myFunction(int x)
+{
+  return 5 + x;
+}
+//
 int main()
 {
 	myFunction();
 	myFunction("Eyal");
+	cout << myFunction(3);
 
 }
