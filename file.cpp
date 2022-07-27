20615a2a503824b72717bfcf59469913a39d8224
Eyal <87993877+eyal78@users.noreply.github.com>
2022-07-27 12:46:25 +0300

    Create file.cpp

---
+++ file.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <fstream>   // A directory for files.
+
+using namespace std;
+
+int main()
+{
+	ofstream eyal("1.txt");    // Create or if already created then open a file.
+}
